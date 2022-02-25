14df707e3d0f17f3cd722df3b91449a33ca03c83
WesLemon <wesleyshaw25@gmail.com>
2022-02-25 17:11:16 +0000

    trying to ge the google login working to no avail

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,38 +1,31 @@
 <template>
   <v-container>
-    
     <v-row class="text-center">
-
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to CaptionBoard
         </h1>
-
         <section id="whenSignedOut">
           <button id="signInBtn" class="btn btn-primary">Sign in with Google</button>
         </section>
-
         <section id="whenSignedIn" hidden>
           <div id="userDetails"></div>
           <button id="signOutBtn" class="btn btn-primary">Sign Out</button>
         </section>
-
-        <script src="login.js" defer></script>
       </v-col>
-
-      
     </v-row>
   </v-container>
 </template>
 
 <script>
   export default {
+    mounted() {
+      let Script = document.createElement("script");
+      Script.setAttribute("src", "src/scripts/login.js");
+      document.head.appendChild(Script);
+    },
     name: 'TestPage',
-
     data: () => ({
-      
-       
-
     }),
   }
 </script>
