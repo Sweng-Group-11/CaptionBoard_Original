50ca531e10b345e626c1a11e7464e75bbbd5f12b
Matthew Dowse <dowsema@tcd.ie>
2022-02-25 14:21:56 +0000

    Revert "added dashboard, login and register components"
    This reverts commit 8da483c3b6a69fa5adbaab2b8ff5d3f3be47e5ff.

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import Router from 'vue-router';
-import Login from '../components/Login'
-import Register from '../components/Register'
-import Dashboard from "@/components/Dashboard";
-
-
