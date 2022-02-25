8da483c3b6a69fa5adbaab2b8ff5d3f3be47e5ff
WesLemon <wesleyshaw25@gmail.com>
2022-02-25 12:25:28 +0000

    added dashboard, login and register components

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import Router from 'vue-router';
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Dashboard from "@/components/Dashboard";
+
+
